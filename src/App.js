5cae5624f8a2bf91a9c1aea211c85a2466f3d40d
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-06-10 22:30:31 +0530

    OTP UI done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import SigninPage from "./components/Signin page/SigninPage";
 import SignupPage from "./components/Signup page/SignupPage";
+import Otp from "./components/otp/Otp";
 
 function App() {
   return (
     <div className="App">
-      <SignupPage/>
+      {/* <SignupPage/> */}
       {/* <SigninPage/> */}
+      <Otp/>
     </div>
   );
 }
