4a80a6cdf19bff562e46ec879c0556733e34dcd8
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-06-11 21:33:15 +0530

    font is changed to poppins

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import App from './App';
 import { Provider } from 'react-redux';
 import {store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { createMuiTheme , ThemeProvider } from '@mui/material';
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Poppins',
+    ].join(','),
+  },});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
     <App />
     </PersistGate>
     </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
